3b4afb231c1217c5f0333620719354dbc059e2d3
Dabu_10 <407790750@qq.com>
2024-11-15 15:05:20 -0500

    1

--- Source/knight.lua
+++ Source/knight.lua
@@ -8,8 +8,8 @@ class('Knight').extends(Player)
 function Knight:init(x,y)
     local knightImage = gfx.image.new("images/knight")
     Knight.super.init(self, x, y, knightImage)
-    self.moveSpeed = 0.1
-    self.projectileSpeed = 0.1
+    self.moveSpeed = 1.5
+    self.projectileSpeed = 1.5
     
 
 end
